b3a240a75297b73c86715be2d1822ce7ac4a664a
nachodeinza <igde267@vidalibarraquer.net>
2024-05-07 09:22:15 +0200

    alpha 0.1.1

--- src/main/java/com/mycompany/llistacontinguts_nachodeinza/PrimaryController.java
+++ src/main/java/com/mycompany/llistacontinguts_nachodeinza/PrimaryController.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
