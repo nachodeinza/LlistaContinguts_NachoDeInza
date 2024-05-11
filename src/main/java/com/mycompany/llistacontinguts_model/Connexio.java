12884d5e0d293e7ae9c53c95253116fce385159d
Nacho <Nacho@DESKTOP-VO28VMG>
2024-05-12 01:09:21 +0200

    Color de fons actualitzat

--- src/main/java/com/mycompany/llistacontinguts_model/Connexio.java
+++ src/main/java/com/mycompany/llistacontinguts_model/Connexio.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Connexio {
 
     //private final String URL = "jdbc:mysql://localhost:3306/dam_bd";//nom bd
-    private final String URL = "jdbc:mysql://localhost:3306/LlistaContinguts_db";//nom bd 
+    private final String URL = "jdbc:mysql://localhost/LlistaContinguts_db";//nom bd 
     private final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private final String USER = "root";
     private final String PASSWD = "";   
